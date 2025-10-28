76b1b85ca75ad5ff3bde16c654213a4702213009
ayushsingh991399 <ayushkumar991399@gmail.com>
2025-10-28 16:42:27 +0530

    update

--- hw5/predict1.py
+++ hw5/predict1.py
@@ -1,14 +1,14 @@
+
 import pickle
 
 with open("pipeline_v1.bin", "rb") as f_in:
-    model = pickle.load(f_in)
+    pipeline = pickle.load(f_in)
 
-client = {
+customer = {
     "lead_source": "paid_ads",
     "number_of_courses_viewed": 2,
     "annual_income": 79276.0
 }
 
-X = [client]
-pred = model.predict_proba(X)[0, 1]
-print(pred)
+prob = pipeline.predict_proba(customer)[0,1]
+print("Probability that lead will convert : ", prob)
--- hw5/test.py
+++ hw5/test.py
@@ -1,11 +1,10 @@
 import requests
 
-url = "http://127.0.0.1:8000/predict"
+url = "http://127.0.0.1:3000/predict"
 client = {
     "lead_source": "organic_search",
     "number_of_courses_viewed": 4,
     "annual_income": 80304.0
 }
 
-response = requests.post(url, json=client).json()
-print(response)
+print(requests.post(url, json=client).json())
